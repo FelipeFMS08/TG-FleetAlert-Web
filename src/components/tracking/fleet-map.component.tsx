ceeb63532b359cebe51a64ca627ac07a9999185d
Felipe Messias <felipe.messias.fms@gmail.com>
2024-09-25 16:17:47 -0300

    Fixed theme colors for map

--- src/components/tracking/fleet-map.component.tsx
+++ src/components/tracking/fleet-map.component.tsx
@@ -33,7 +33,7 @@ const FleetMap = () => {
                 L.marker(endPoint).addTo(map);
                 L.marker(startPoint).addTo(map);
 
-                L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+                L.tileLayer((theme == 'light' ? `https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png`: `https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png`), {
                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 }).addTo(map);
 
