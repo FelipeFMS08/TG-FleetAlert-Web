39d17e24759b744b04fb2fb6156fbc330ae67b2c
Felipe Messias <felipe.messias.fms@gmail.com>
2024-09-25 15:49:28 -0300

    Fixed the window and width map content

--- src/components/tracking/fleet-map.component.tsx
+++ src/components/tracking/fleet-map.component.tsx
@@ -1,87 +1,78 @@
 import { useEffect, useRef, useState } from "react";
-import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
-import { MapContainer, TileLayer } from "react-leaflet";
+import L from 'leaflet';
 
 const FleetMap = () => {
-    const mapRef = useRef<L.Map | null>(null);
     const [estimatedTime, setEstimatedTime] = useState<number | null>(null);
+    const mapRef = useRef<L.Map | null>(null);
+    const startPoint: [number, number] = [-23.4422149, -46.9243186]; 
+    const endPoint: [number, number] = [-23.4065828, -46.8806725]; 
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            if (mapRef.current) return;
+    const SetupRouting = () => {
+        const map = useMap(); 
+        useEffect(() => {
+            if (!mapRef.current) {
+                mapRef.current = map;
 
-            const DefaultIcon = L.icon({
-                iconUrl: '/leaflet/marker-icon.png',
-                shadowUrl: '/leaflet/marker-shadow.png',
-                iconSize: [25, 25],
-                iconAnchor: [12.5, 25],
-                shadowSize: [41, 41],
-            });
-            L.Marker.prototype.options.icon = DefaultIcon;
+                const DefaultIcon = L.icon({
+                    iconUrl: '/leaflet/marker-icon.png',
+                    shadowUrl: '/leaflet/marker-shadow.png',
+                    iconSize: [25, 25],
+                    iconAnchor: [12.5, 25],
+                    shadowSize: [41, 41],
+                });
+                L.Marker.prototype.options.icon = DefaultIcon;
 
-            const map = L.map('map').setView([-23.561682, -46.655898], 13);
-            mapRef.current = map;
+                L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                }).addTo(map);
 
-            L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            }).addTo(map);
+                L.marker(endPoint).addTo(map);
+                L.marker(startPoint).addTo(map);
 
-            const startPoint: [number, number] = [-23.4422149, -46.9243186];
-            const endPoint: [number, number] = [-23.4065828, -46.8806725];
+                const routingControl = L.Routing.control({
+                    waypoints: [
+                        L.latLng(startPoint[0], startPoint[1]),
+                        L.latLng(endPoint[0], endPoint[1]),
+                    ],
+                    lineOptions: {
+                        styles: [{ color: '#ef4444', opacity: 0.7, weight: 5 }],
+                        extendToWaypoints: true,
+                        missingRouteTolerance: 10
+                    },
+                    show: false
+                }).addTo(map);
 
-            L.marker(endPoint).addTo(map);
-            L.marker(startPoint).addTo(map);
+                routingControl.on('routesfound', (e) => {
+                    const routes = e.routes;
+                    const route = routes[0];
 
-            const routingControl = L.Routing.control({
-                waypoints: [
-                    L.latLng(startPoint[0], startPoint[1]),
-                    L.latLng(endPoint[0], endPoint[1]),
-                ],
-                lineOptions: {
-                    styles: [{ color: '#ef4444', opacity: 0.7, weight: 5 }],
-                    extendToWaypoints: true,
-                    missingRouteTolerance: 10
-                },
-                show: false
-            }).addTo(map);
+                    setEstimatedTime(route.summary.totalTime);
 
+                    const latitudes = route.coordinates.map((coord: any) => coord.lat);
+                    const longitudes = route.coordinates.map((coord: any) => coord.lng);
 
+                    const center = [
+                        (Math.min(...latitudes) + Math.max(...latitudes)) / 2,
+                        (Math.min(...longitudes) + Math.max(...longitudes)) / 2,
+                    ] as L.LatLngExpression;
 
-            routingControl.on('routesfound', (e) => {
-                const routes = e.routes;
-                const route = routes[0];
+                    const radius = Math.max(
+                        ...route.coordinates.map((coord: any) => map.distance(center, [coord.lat, coord.lng]))
+                    );
 
-                setEstimatedTime(route.summary.totalTime);
+                    L.circle(center as L.LatLngExpression, { radius, color: 'gray', fillOpacity: 0.2 }).addTo(map);
 
-                const latitudes = route.coordinates.map((coord: any) => coord.lat);
-                const longitudes = route.coordinates.map((coord: any) => coord.lng);
-
-                const center = [
-                    (Math.min(...latitudes) + Math.max(...latitudes)) / 2,
-                    (Math.min(...longitudes) + Math.max(...longitudes)) / 2,
-                ] as L.LatLngExpression;
-
-                const radius = Math.max(
-                    ...route.coordinates.map((coord: any) => map.distance(center, [coord.lat, coord.lng]))
-                );
-
-                L.circle(center as L.LatLngExpression, { radius, color: 'gray', fillOpacity: 0.2 }).addTo(map);
-
-                const bounds = L.latLngBounds([startPoint, endPoint]);
-                map.fitBounds(bounds);
-            });
-
-            return () => {
-                if (mapRef.current) {
-                    mapRef.current.remove();
-                    mapRef.current = null;
-                }
-            };
-        }
-    }, []);
+                    const bounds = L.latLngBounds([startPoint, endPoint]);
+                    map.fitBounds(bounds);
+                });
+            }
+        }, [map]); 
+        return null;
+    };
 
     return (
         <MapContainer
@@ -91,10 +82,12 @@ const FleetMap = () => {
             zoom={13}
         >
             <TileLayer
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             />
-            <div id="map" style={{ height: "100%", width: "100%" }} />
+            <Marker position={startPoint} />
+            <Marker position={endPoint} />
+            <SetupRouting />
         </MapContainer>
     );
 };
